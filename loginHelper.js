34ef0483e322e217bfcdc788c9f2057ebe0c91ba
khzheng24998 <kennyzheng24998@gmail.com>
2019-01-19 01:28:36 -0800

    Can now query database to check if user login is valid
    Next step: Begin backend rewrite

--- loginHelper.js
+++ loginHelper.js
@@ -2,31 +2,68 @@
 const Database = require("./database.js");
 
 //npm modules
+const crypto = require("crypto");
 const bcrypt = require("bcrypt");
 const saltRounds = 12;
 
-// Exported functions
+/* Helper functions */
+
+//Return value: A Promise which resolves to a boolean
+function checkPassword(password, hash)
+{
+	return bcrypt.compare(password, hash).then(res => res);
+}
+
+//Return value: A string
+function asyncKeyGen(type)
+{
+	let attempts = 0;
+	while (attempts < 50)
+	{
+		let key = crypto.randomBytes(24).toString('hex');
+		let entity = {};
+		/*switch (type)
+		{
+			case "session":
+				entity = await Database.findActiveSession("key", key);
+				break;
+			case "reset":
+				entity = await Database.findResetRequest("key", key);
+				break;
+			case "verification":
+				entity = await Database.findVerificationRequest("key", key);
+				break;
+			default:
+				console.log("ERROR: asyncKeyGen() - Invalid type.");
+				break;
+		}
+
+		if (entity === null)*/
+		return key;
+		attempts++;
+	}
+}
+
+/* Exported functions */
 
 async function asyncLogIn(req, res)
 {
 	let body = req.body;
-
 	let user = await Database.findUser("email", body.email);
 	if (user === null)
 	{
-		console.log("User not found!");
 		res.send({ msg: "invalid-credentials" });
 		return;
 	}
 
-	/*let validCredentials = await asyncComparePasswords(body.password, user.userInfo.password);
-	if (!validCredentials)
+	let valid = await checkPassword(body.password, user.userInfo.password);
+	if (!valid)
 	{
 		res.send({ msg: "invalid-credentials" });
 		return;
 	}
 
-	let key = await asyncKeyGen("session");
+	/*let key = await asyncKeyGen("session");
 	asyncActiveSessionGen(key, user._id, user.userInfo);
 
 	res.cookie("loginKey", key);*/
