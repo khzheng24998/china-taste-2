5ed5020c5099211a43c0aa76cecd1d7e227e9738
khzheng24998 <kennyzheng24998@gmail.com>
2019-01-28 21:56:33 -0800

    Experimenting with getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,66 @@
 import NavBar from "../components/NavBar.js";
 import Footer from "../components/Footer.js";
+import Link from 'next/link';
 
-const Index = () => (
-  <div>
-    <style jsx global>{`
-      body {
-        margin: 0
-      }
-    `}</style>
+const categoryStles = {
+  border: "solid 2px #999999",
+  width: 150
+}
 
-    <NavBar />
+const bodyStyles = {
+  margin: "0 auto",
+  width: "80%",
+}
 
-    <div style={{height: 1000}}>Hello Next.js</div>
+const PostLink = (props) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
 
-    <Footer />
-  </div>
-)
+class Index extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+  }
+
+  static async getInitialProps({ req })
+  {
+    if(req)
+    {
+        // called on server
+    }
+
+    else
+    {
+        // called on client
+    }
+
+    return {};
+  }
+
+  render()
+  {
+    return (<div>
+      <style jsx global>{` body { margin: 0 } `}</style>
+      <NavBar />
+      <br/><br/><br/>
+
+      <div style={bodyStyles}>
+
+      <div id="categories" style={categoryStles}>
+        <p>Appetizers</p>
+        <p>Soup</p>
+      </div>
+
+      </div>
+
+      <Footer />
+    </div>);
+  }
+}
 
 export default Index;
